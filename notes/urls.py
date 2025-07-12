11bb5f772565f3c0b7ab6bebf178e9b5201e9b9c
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2025-07-13 00:55:08 +0330

    fix: ghabli

--- notes/urls.py
+++ notes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.ListCreateNoteAPIView.as_view(), name='get_post_notes'),
     path('bulk', views.BulkCreateNoteAPIView.as_view(), name='batch_create_notes'),
     path('filter', views.FilterNotesAPIView.as_view(), name='get_filter_notes'),
-    path('<int:pk>/', views.RetrieveUpdateDestroyNoteAPIView.as_view(), name='get_delete_update_note'),
+    path('<int:user_id>/', views.RetrieveUpdateDestroyNoteAPIView.as_view(), name='get_delete_update_note'),
     path('get_update/', views.GetUpdateNoteAPIView.as_view(), name='get_update_note'),
 ]
